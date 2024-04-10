3333c83ae27c6c012aef2df01cea3a05f5bac734
SilentAssassin101 <treytonwilkerson@gmail.com>
2024-04-10 09:41:30 -0400

    initial

---
+++ src/components/ArticleItem.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="my-2 p-3 bg-base-200 rounded-lg shadow-lg border-t-2 border-primary">
+        <h2 class="text-4xl">{{title}}</h2>
+        <p class="text-lg text-left">{{content}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="flex flex-col justify-center text-center max-w-2xl mx-auto gap-2 px-2 pt-2">
+        <h1 class="text-5xl">Funny Government Activities</h1>
+        <ArticleItem title="9/11" content="aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a" />
+        <ArticleItem title="JFK" content="aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a" />
+        <ArticleItem title="Ruby Ridge" content="aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a" />
+        <ArticleItem title="WACO" content="aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a aD adijw jdj jsad w9 jdjas dj wj aks jdw j nsngj a" />
     </div>
 </template>
 
 <script>
+import ArticleItem from '../components/ArticleItem.vue'
 export default {
     name: 'ViewHome',
+    components: {
+        ArticleItem
+    }
 }
 </script>
-
+    
 <style lang="scss" scoped></style>
